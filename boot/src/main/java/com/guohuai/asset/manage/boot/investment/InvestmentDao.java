843021c241ce427d07a029986437e3183223b757
Administrator <Administrator@USER-20160319BN>
2016-05-17 09:28:21 +0800

    投资标的entity,dao和service

---
+++ boot/src/main/java/com/guohuai/asset/manage/boot/investment/InvestmentDao.java
@@ -0,0 +1,9 @@
+package com.guohuai.asset.manage.boot.investment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface InvestmentDao
+		extends JpaRepository<Investment, String>, JpaSpecificationExecutor<Investment> {
+
+}
