7af63c1f01063f247c9dd486ac9a665daaa942f0
Scott Sut <scottsut@163.com>
2016-05-17 13:22:45 +0800

    新建标的弹窗，标的明细弹窗

--- client/pages/targetApply/targetApply.js
+++ client/pages/targetApply/targetApply.js
@@ -9,6 +9,10 @@ define([
     init: function () {
       // js逻辑写在这里
 
+      // 新建标的按钮点击事件
+      $('#targetAdd').on('click', function () {
+        $('#addTargetModal').modal('show')
+      })
     }
   }
 })
