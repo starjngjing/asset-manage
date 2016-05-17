58c9a75f9517f9f4652f218f57de0e8e04fdd893
vania910 <wangyi@guohuaigroup.com>
2016-05-17 10:50:29 +0800

    modify files

--- boot/src/main/java/com/guohuai/asset/manage/boot/investment/InvestmentPoolController.java
+++ boot/src/main/java/com/guohuai/asset/manage/boot/investment/InvestmentPoolController.java
@@ -1,7 +1,18 @@
 package com.guohuai.asset.manage.boot.investment;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guohuai.asset.manage.component.web.BaseController;
@@ -10,6 +21,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Extension;
 import io.swagger.annotations.ExtensionProperty;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 
 
 
@@ -25,12 +40,32 @@ import io.swagger.annotations.ExtensionProperty;
 @RequestMapping("/asset/hill/investmentPool")
 @Api("投资标的库操作相关接口")
 public class InvestmentPoolController extends BaseController {
-	@ApiOperation(value = "登录", extensions = {
+	@Autowired
+	InvestmentService investmentService;
+		
+
+	@RequestMapping(value = "listinvestment", method = { RequestMethod.POST, RequestMethod.GET })
+	@ApiOperation(value = "标的成立管理列表", extensions = {
 			@Extension(properties = { @ExtensionProperty(name = "x-code", value = "1001,1002"),
 					@ExtensionProperty(name = "x-ref", value = "innerResp") }) })
-	@RequestMapping(method = RequestMethod.GET)
-	public void listinvestment() {
-
+	public @ResponseBody ResponseEntity<InvestmentListResp> listinvestment(HttpServletRequest request,
+			@And({ @Spec(path = "projectName", spec = Like.class), @Spec(path = "projectManager", spec = Equal.class),
+					@Spec(path = "pjType", params = "pjType", spec = Equal.class) }) Specification<Investment> spec,
+			@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "50") int size, @RequestParam(defaultValue = "desc") String sortDirection,
+			@RequestParam(defaultValue = "updateTime") String sortField) {
+		if (page < 1) {
+			page = 1;
+		}
+		if (size <= 0) {
+			size = 50;
+		}
+		Pageable pageable = new PageRequest(page, size);
+		
+		Page<Investment> pageData = investmentService.getInvestmentList(spec, pageable);
+		
+		
+		InvestmentListResp resp = new InvestmentListResp(pageData);
+		return new ResponseEntity<InvestmentListResp>(resp, HttpStatus.OK);
 	}
 	
 }
