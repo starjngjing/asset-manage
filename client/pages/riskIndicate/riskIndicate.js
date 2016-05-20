3f1fff9a85186f0d3f51cfe9193c47c3653ba07c
Scott Sut <scottsut@163.com>
2016-05-20 15:01:44 +0800

    添加风险关键指标维护页html

---
+++ client/pages/riskIndicate/riskIndicate.js
@@ -0,0 +1,15 @@
+/**
+ * 风险关键指标维护
+ */
+define([
+  'util',
+  'extension'
+], function (util, $$) {
+  return {
+    name: 'riskIndicate',
+    init: function () {
+      // js逻辑写在这里
+
+    }
+  }
+})
