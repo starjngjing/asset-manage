41d6466594a1c634ae5f472e20f5fa89c2681266
vania910 <wangyi@guohuaigroup.com>
2016-05-21 09:03:25 +0800

    添加功能

---
+++ client/pages/cashtoolHistory/cashtoolHistory.js
@@ -0,0 +1,212 @@
+/**
+ * 现金类管理工具备选库
+ */
+define([
+'http',
+'config',
+'extension',
+'util'
+], function (http, config, $$, util) {
+  return {
+    name: 'cashtoolStorage',
+    init: function () {
+      // js逻辑写在这里
+
+        // 分页配置
+        var pageOptions = {
+        		page: 1,
+          rows: 10
+        }
+        // 数据表格配置
+        var tableConfig = {
+          ajax: function (origin) {
+            http.post(config.api.listCashTool, {
+              data: pageOptions,
+              contentType: 'form'
+            }, function (rlt) {
+              origin.success(rlt)
+            })
+          },
+          pageNumber: pageOptions.page,
+          pageSize: pageOptions.rows,
+          pagination: true,
+          sidePagination: 'server',
+          pageList: [10, 20, 30, 50, 100],
+          queryParams: getQueryParams,
+          onLoadSuccess: function () {},
+          columns: [
+            {// 代码
+            	field: 'ticker',
+//              width: 60,
+              align: 'center'
+              
+            },
+            {// 基金名称
+            	field: 'secShortName',
+//              width: 60,
+            	align: 'center'
+            		
+            },
+            {// 类型
+//            	width: 60,
+              field: 'etfLof',
+              formatter: function (val) {
+            	return util.enum.transform('CASHTOOLTYPE', val);
+              }
+            },
+            {// 最新流通份额
+            	field: 'circulationShares',
+            	formatter: function (val) {
+            		return val;
+            	}
+            },
+            {// 7日年化收益率
+            	field: 'weeklyYield',
+            	formatter: function (val) {
+            		if(val)
+            			return val.toFixed(2) + "%";
+            		return val;
+            	}
+            },
+            { // 状态
+            	field: 'state',
+            	formatter: function (val) {
+            		return val;
+            	}
+            },
+            { // 持有份额
+            	field: 'holdAmount',
+            	formatter: function (val) {
+            		return val;
+            	}
+            },
+            {
+//              field: 'operator',
+              width: 260,
+              align: 'center',
+              formatter: function (val) {
+            	  var buttons = [
+            	    {
+            	      text: '移除出库',
+            	      type: 'button',
+            	      class: 'item-remove',
+            	      isRender: true
+            	    },
+              	    {
+              	    	text: '收益采集',
+              	    	type: 'button',
+              	    	class: 'item-cashToolRevenue',
+              	    }
+            	  ];
+            	  return util.table.formatter.generateButton(buttons);
+              },
+              events: {
+                  'click .item-remove': function (e, value, row) { // 移除出库
+                	  $("#confirmTitle").html("确定移除现金管理工具？")
+						$$.confirm({
+							container: $('#doConfirm'),
+							trigger: this,
+							accept: function() {
+								http.post(config.api.removeCashTool, {
+									data: {
+										oid: row.oid
+									},
+									contentType: 'form'
+								}, function(result) {
+									if (result.errorCode == 0) {
+										$('#dataTable').bootstrapTable('refresh');
+									} else {
+										alert('移除现金管理工具失败');
+									}
+								})
+							}
+						})
+                	  
+                  },
+                  'click .item-cashToolRevenue': function (e, value, row) { // 收益采集-显示弹窗
+                	  http.post(config.api.cashtoolDetQuery, {
+                		  data: {
+                			  oid:row.oid
+                		  },
+                		  contentType: 'form'
+                	  },
+                	  function (obj) {
+                		  var data = obj.data;
+                		  if(!data){
+                			  toastr.error('现金管理工具详情数据不存在', '错误信息', {
+                				  timeOut: 10000
+                			  });
+                		  }
+                		  data.cashtoolOid = data.oid; // 手动为 cashtoolOid 赋值
+                		  $$.detailAutoFix($('#cashToolDetail'), data);	// 自动填充详情
+                		  $$.formAutoFix($('#cashToolRevenueForm'), data); // 自动填充表单
+                	  });
+                	  $('#cashToolRevenueModal').modal('show');
+                  },
+                  'click .item-detail': function (e, value, row) { // 详情
+                    http.post(config.api.applyGetUserInfo, {
+                      data: {
+                        aoid: row.oid
+                      },
+                      contentType: 'form'
+                    }, function (result) {
+                      
+                      $('#detailModal').modal('show')
+                    })
+                  }
+                
+              }
+            }
+          ]
+        }
+
+        // 初始化数据表格
+        $('#dataTable').bootstrapTable(tableConfig)
+        // 搜索表单初始化
+        $$.searchInit($('#searchForm'), $('#dataTable'))
+        
+        // 收益采集 按钮点击事件
+        $("#cashToolRevenueSubmit").click(function(){
+        	$("#cashToolRevenueForm").ajaxSubmit({
+        		type:"post",  //提交方式  
+                //dataType:"json", //数据类型'xml', 'script', or 'json'  
+        		url: config.api.cashToolRevenueSave,
+        		success:function(data) {
+        			$('#cashToolRevenueForm').clearForm();
+        			$('#cashToolRevenueModal').modal('hide');
+        			$('#dataTable').bootstrapTable('refresh');
+        		}
+        	});
+        	
+        });
+        
+        // 修改按钮点击事件
+        $("#unEstablishSubmit").click(function(){
+        	$("#unEstablishForm").ajaxSubmit({
+        		type:"post",  //提交方式  
+        		//dataType:"json", //数据类型'xml', 'script', or 'json'  
+        		url: config.api.unEstablish,
+        		success:function(data) {
+        			$('#unEstablishForm').clearForm();
+        			$('#unEstablishModal').modal('hide');
+        			$('#dataTable').bootstrapTable('refresh');
+        		}
+        	});
+        	
+        });
+
+        function getQueryParams (val) {
+          var form = document.searchForm
+          pageOptions.ticker = form.ticker.value;
+          pageOptions.secShortName = form.secShortName.value;
+          pageOptions.etfLof = form.etfLof.value;
+          pageOptions.circulationShares = form.circulationShares.value;
+          pageOptions.weeklyYield = form.weeklyYield.value;
+          pageOptions.rows = val.limit
+          pageOptions.page = parseInt(val.offset / val.limit) + 1
+          return val
+        }
+      
+    }
+  }
+})
---
+++ client/pages/targetHistory/targetHistory.js
@@ -0,0 +1,271 @@
+/**
+ * 投资标的备选库
+ */
+define([
+'http',
+'config',
+'extension',
+'util'
+], function (http, config, $$, util) {
+  return {
+    name: 'targetStorage',
+    init: function () {
+      // js逻辑写在这里
+
+        // 分页配置
+        var pageOptions = {
+        		page: 1,
+          rows: 10
+        }
+        // 数据表格配置
+        var tableConfig = {
+          ajax: function (origin) {
+            http.post(config.api.listinvestment, {
+              data: pageOptions,
+              contentType: 'form'
+            }, function (rlt) {
+              origin.success(rlt)
+            })
+          },
+          pageNumber: pageOptions.page,
+          pageSize: pageOptions.rows,
+          pagination: true,
+          sidePagination: 'server',
+          pageList: [10, 20, 30, 50, 100],
+          queryParams: getQueryParams,
+          onLoadSuccess: function () {
+          //  http.post(config.api.listinvestment, {
+           //   contentType: 'form'
+           // }, function (result) {
+//              $('#clubData').html('会员机构：' + result.clubData + '家')
+//              $('#platAssetData').html('平台资产：' + result.platAssetData + '项')
+//              $('#assetSizeData').html('资产规模：' + result.assetSizeData + '亿')
+//              $('#updateTime').html('变更时间：' + util.table.formatter.timestampToDate(result.updateTime, 'YYYY-MM-DD'))
+//              document.clubDataForm.data.value = result.clubData
+//              document.platAssetDataForm.data.value = result.platAssetData
+//              document.assetSizeDataForm.data.value = result.assetSizeData
+//              $('#clubDataForm').validator()
+//              $('#platAssetDataForm').validator()
+//              $('#assetSizeDataForm').validator()
+            //})
+          },
+          columns: [
+            {// 名称
+            	field: 'name',
+//              width: 60,
+              align: 'center'
+              
+            },
+            {// 类型
+//            	width: 60,
+              field: 'type',
+              formatter: function (val) {
+            	return util.enum.transform('TARGETTYPE', val);
+              }
+            },
+            {// 收益率
+            	field: 'expAror',
+            	formatter: function (val) {
+            		if(val)
+						return val.toFixed(2) + "%";
+            		return val;
+            	}
+            },
+            {
+            	// 标的规模
+            	field: 'raiseScope',
+            	formatter: function (val) {
+            		return val;
+            	}
+            },
+            { // 标的限期（日）
+              field: 'lifed',
+              
+            },
+            { // 状态
+            	field: 'state',
+            	formatter: function (val) {
+            		return val;
+            	}
+            },
+            { // 已购份额
+            	field: 'holdAmount',
+            	formatter: function (val) {
+            		return val;
+            	}
+            },
+            {
+//              field: 'operator',
+              width: 260,
+              align: 'center',
+              formatter: function (val) {
+            	  var buttons = [
+            	    {
+            	      text: '成立',
+            	      type: 'button',
+            	      class: 'item-establish',
+            	      isRender: true
+            	    },
+            	    {
+              	      text: '不成立',
+              	      type: 'button',
+              	      class: 'item-unEstablish',
+              	      isRender: true
+              	    },
+              	    {
+              	    	text: '本息兑付',
+              	    	type: 'button',
+              	    	class: 'item-targetIncome',
+//              	    	isRender: row.state == 'establish',
+              	    	isRender: true,
+              	    },
+              	    {
+              	    	text: '财务数据',
+              	    	type: 'button',
+              	    	class: 'item-financialData',
+              	    	//isRender: row.state == 'establish',
+              	    	isRender: true,
+              	    }
+            	  ];
+            	  return util.table.formatter.generateButton(buttons);
+              },
+              events: {
+                  'click .item-establish': function (e, value, row) {
+                	  // 初始化   付息日 
+                      for ( var i = 1; i <= 30; i++) {
+	                      	var option = $("<option>").val(i).text(i);
+	                      	$(establishForm.accrualDate).append(option);
+	              		}
+                      $(establishForm.accrualDate).val(10); // 默认10个工作日
+                	  
+                	http.post(config.api.targetDetQuery, {
+                        data: {
+                        	oid:row.oid
+                        },
+                        contentType: 'form'
+                      },
+                	  function (obj) {
+                    	  var data  = obj.investment;
+                    	  if(!data){
+                    		  toastr.error('标的详情数据不存在', '错误信息', {
+                    			    timeOut: 10000
+                    			  });
+                    	  }
+                	  $$.detailAutoFix($('#establishForm'), data);	// 自动填充详情
+                	  $$.formAutoFix($('#establishForm'), data); // 自动填充表单
+                	});
+                	$('#establishModal').modal('show');
+                  },
+                  'click .item-unEstablish': function (e, value, row) {
+                	  http.post(config.api.targetDetQuery, {
+                		  data: {
+                			  oid:row.oid
+                		  },
+                		  contentType: 'form'
+                	  },
+                	  function (obj) {
+                		  var data  = obj.investment;
+                		  if(!data){
+                			  toastr.error('标的详情数据不存在', '错误信息', {
+                				  timeOut: 10000
+                			  });
+                		  }
+                		  $$.detailAutoFix($('#unEstablishForm'), data);	// 自动填充详情
+                		  $$.formAutoFix($('#unEstablishForm'), data); // 自动填充表单
+                	  });
+                	  $('#unEstablishModal').modal('show');
+                  },
+                  'click .item-targetIncome': function (e, value, row) {
+                	  http.post(config.api.targetDetQuery, {
+                		  data: {
+                			  oid:row.oid
+                		  },
+                		  contentType: 'form'
+                	  },
+                	  function (obj) {
+                		  var data  = obj.investment;
+                		  if(!data){
+                			  toastr.error('标的详情数据不存在', '错误信息', {
+                				  timeOut: 10000
+                			  });
+                		  }
+                		  $$.detailAutoFix($('#targetIncomeForm'), data);	// 自动填充详情
+                		  $$.formAutoFix($('#targetIncomeForm'), data); // 自动填充表单
+                	  });
+                	  $('#targetIncomeModal').modal('show');
+                  },
+                  'click .item-financialData': function(e, value, row) {
+                	  
+                  }
+                
+              }
+            }
+          ]
+        }
+
+        // 初始化数据表格
+        $('#dataTable').bootstrapTable(tableConfig)
+        // 搜索表单初始化
+        $$.searchInit($('#searchForm'), $('#dataTable'))
+        
+        
+        // 成立 按钮点击事件
+        $("#establishSubmit").click(function(){
+        	$("#establishForm").ajaxSubmit({
+        		type:"post",  //提交方式  
+                //dataType:"json", //数据类型'xml', 'script', or 'json'  
+        		url: config.api.establish,
+        		success:function(data) {
+        			$('#establishForm').clearForm();
+        			$('#establishModal').modal('hide');
+        			$('#dataTable').bootstrapTable('refresh');
+        		}
+        	});
+        	
+        });
+        
+        // 不成立 按钮点击事件
+        $("#unEstablishSubmit").click(function(){
+        	$("#unEstablishForm").ajaxSubmit({
+        		type:"post",  //提交方式  
+        		//dataType:"json", //数据类型'xml', 'script', or 'json'  
+        		url: config.api.unEstablish,
+        		success:function(data) {
+        			$('#unEstablishForm').clearForm();
+        			$('#unEstablishModal').modal('hide');
+        			$('#dataTable').bootstrapTable('refresh');
+        		}
+        	});
+        	
+        });
+        
+        // 本息兑付 按钮点击事件
+        $("#targetIncomeSubmit").click(function(){
+        	$("#targetIncomeForm").ajaxSubmit({
+        		type:"post",  //提交方式  
+        		//dataType:"json", //数据类型'xml', 'script', or 'json'  
+        		url: config.api.targetIncomeSave,
+        		success:function(data) {
+        			$('#targetIncomeForm').clearForm();
+        			$('#targetIncomeModal').modal('hide');
+        			$('#dataTable').bootstrapTable('refresh');
+        		}
+        	});
+        	
+        });
+
+        function getQueryParams (val) {
+          var form = document.searchForm
+          pageOptions.name = form.name.value;
+          pageOptions.type = form.type.value;
+          pageOptions.raiseScope = form.raiseScope.value;
+          pageOptions.lifed = form.lifed.value;
+          pageOptions.expAror = form.expAror.value;
+          pageOptions.rows = val.limit
+          pageOptions.page = parseInt(val.offset / val.limit) + 1
+          return val
+        }
+      
+    }
+  }
+})
