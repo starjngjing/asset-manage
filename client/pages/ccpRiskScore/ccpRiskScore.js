8d9ada348ed7b092be01d0a76adfc562f66ad7a5
Scott Sut <scottsut@163.com>
2016-05-21 16:38:17 +0800

    添加信用等级评分模型页html

---
+++ client/pages/ccpRiskScore/ccpRiskScore.js
@@ -0,0 +1,15 @@
+/**
+ * 信用等级评分模型
+ */
+define([
+  'util',
+  'extension'
+], function (util, $$) {
+  return {
+    name: 'ccpRiskScore',
+    init: function () {
+      // js逻辑写在这里
+
+    }
+  }
+})
