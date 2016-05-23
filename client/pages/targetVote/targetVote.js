1d2c35fdf54a6b16f91d3020ef365e22e7b5cc12
Scott Sut <scottsut@163.com>
2016-05-23 10:28:19 +0800

    fix bugs

--- client/pages/targetVote/targetVote.js
+++ client/pages/targetVote/targetVote.js
@@ -60,6 +60,30 @@ define([
 			};
 			// 初始化表格
 			$('#targetVoteTable').bootstrapTable(tableConfig)
+
+			// 过会投票 -> 赞成按钮点击事件
+			$('#doVoteAccept').on('click', function () {
+				$(document.voteForm).ajaxSubmit({
+					url: '',
+					success: function () {
+						$('#voteModal').modal('hide')
+					}
+				})
+			})
+			// 过会投票 -> 不赞成按钮点击事件
+			$('#doVoteReject').on('click', function () {
+
+			})
+			// 初始化过会进程tab上传附件插件
+			$$.uploader({
+				container: $('#voteUploader'),
+				success: function(file) {
+					$('#voteFile').show().find('a').attr('href', 'http://api.guohuaigroup.com' + file.url)
+					$('#voteFile').find('span').html(file.name)
+					document.voteForm.file.value = file.url
+				}
+			})
+
 		}
 	}
 })
