70c67f775c9e4a31c16eb6ae06bb22ea4ff9a5ed
xjj <M1@LAPTOP-D28R5BIJ>
2016-05-29 00:37:00 +0900

    渠道修改

--- client/pages/channel/channel.js
+++ client/pages/channel/channel.js
@@ -239,9 +239,9 @@ function (http, config, util, $$) {
 		  // 初始化数据表格
 		  $('#channelTable').bootstrapTable(tableConfig);
 		  $$.searchInit($('#searchForm'), $('#channelTable'));
-		  //$('#channelForm').validator();
+		 
 		  util.form.validator.init($('#channelForm'))
-			//$('#channelForm').validator('validate')
+			$('#channelForm').validator('validate')
 
 		  function getQueryParams (val) {
         // 参数 val 是bootstrap-table默认的与服务器交互的数据，包含分页、排序数据
