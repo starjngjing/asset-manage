fa344b011f78fe1b136ebd86d379e24c369f1a2d
vania910 <wangyi@guohuaigroup.com>
2016-06-03 10:15:02 +0800

    modify files

--- boot/src/main/java/com/guohuai/asset/manage/boot/investment/pool/TargetIncomeService.java
+++ boot/src/main/java/com/guohuai/asset/manage/boot/investment/pool/TargetIncomeService.java
@@ -1,22 +1,16 @@
 package com.guohuai.asset.manage.boot.investment.pool;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import com.guohuai.asset.manage.boot.cashtool.pool.CashToolRevenue;
-import com.guohuai.asset.manage.boot.investment.Investment;
 import com.guohuai.asset.manage.boot.investment.InvestmentDao;
-import com.guohuai.asset.manage.component.exception.AMPException;
 
 /**
  * 
@@ -35,37 +29,7 @@ public class TargetIncomeService {
 	@Autowired
 	private InvestmentDao investmentDao;
 
-	/**
-	 * 投资标的本息兑付
-	 * @Title: save 
-	 * @author vania
-	 * @version 1.0
-	 * @see:
-	 * @param interestForm
-	 * @return Interest    返回类型
-	 */
-	public TargetIncome save(TargetIncomeForm interestForm) {
-		String targetOid = interestForm.getTargetOid();
-		if (StringUtils.isBlank(targetOid))
-			throw AMPException.getException("投资标的id不能为空");
-		
-		TargetIncome interest = new TargetIncome();
-		BeanUtils.copyProperties(interestForm, interest);		
-
-		Investment investment = this.investmentDao.findOne(targetOid);
-		if (null == investment)
-			throw AMPException.getException("找不到id为[" + targetOid + "]的投资标的");
-		interest.setInvestment(investment);
-		
-		// 保存之前先删除标的某一期本兮兑付的数据
-		targetIncomeDao.deleteByTargetOidAndSeq(targetOid, interestForm.getSeq());
-		
-		interest.setCreateTime(new Timestamp(System.currentTimeMillis()));
-		interest.setUpdateTime(new Timestamp(System.currentTimeMillis()));
-		return targetIncomeDao.save(interest);
-	}
 	
-
 	
 	/**
 	 * 分页查询本息兑付
