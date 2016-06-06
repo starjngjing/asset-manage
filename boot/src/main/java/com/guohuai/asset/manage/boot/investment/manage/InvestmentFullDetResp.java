f3c48a20a22a91949ac801f6b99e437314fc6288
lirong1986 <lirong@guohuaigroup.com>
2016-06-06 17:41:35 +0800

    --

---
+++ boot/src/main/java/com/guohuai/asset/manage/boot/investment/manage/InvestmentFullDetResp.java
@@ -0,0 +1,43 @@
+package com.guohuai.asset.manage.boot.investment.manage;
+
+import java.util.List;
+
+import com.guohuai.asset.manage.boot.investment.Investment;
+import com.guohuai.asset.manage.boot.investment.InvestmentMeeting;
+import com.guohuai.asset.manage.boot.investment.meeting.VoteDetResp;
+import com.guohuai.asset.manage.boot.project.Project;
+import com.guohuai.asset.manage.component.web.view.BaseResp;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+/**
+ * 全属性标的详情response
+ * 
+ * @author Administrator
+ *
+ */
+@Data
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = false)
+public class InvestmentFullDetResp extends BaseResp {
+
+	public InvestmentFullDetResp(Investment investment, List<Project> projects, InvestmentMeeting investmentMeeting,
+			List<VoteDetResp> votes) {
+		super();
+		this.investment = investment;
+		this.projects = projects;
+		this.investmentMeeting = investmentMeeting;
+		this.votes = votes;
+	}
+
+	private Investment investment; // 标的详情
+
+	private List<Project> projects; // 底层项目详情
+
+	private InvestmentMeeting investmentMeeting; // 过会详情
+
+	private List<VoteDetResp> votes; // 过会表决详情
+}
