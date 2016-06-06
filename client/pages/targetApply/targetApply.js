8207557eac72702e87ab74e668bf160da0e36c65
vania910 <wangyi@guohuaigroup.com>
2016-06-06 16:04:44 +0800

    fix bug

--- client/pages/targetApply/targetApply.js
+++ client/pages/targetApply/targetApply.js
@@ -500,13 +500,15 @@ define([
 				
 				$$.detailAutoFix($('#targetDetail'), formatTargetData(targetInfo)); // 自动填充详情
 
-				$('#projectForm').clearForm(); // 先清理表单
+				$('#projectForm').resetForm(); // 先清理表单
 
 				//初始化:担保方式下拉列表,抵押方式下拉列表,质押方式下拉列表	 
 				initSel();
 
 				// 给项目表单的 标的id属性赋值
-				$("#targetOid")[0].value = targetInfo.oid;
+				//$("#targetOid")[0].value = targetInfo.oid;
+				$(document.projectForm.targetOid).val(targetInfo.oid); // 给项目表单的 标的id属性赋值
+				$(document.projectForm.oid).val(''); // 重置项目oid
 				$('#projectModal').modal('show');
 			})
 
