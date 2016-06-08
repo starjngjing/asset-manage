61b93bce4ea5fba1eb6d2ac5a95e17c0a4019226
lirong1986 <lirong@guohuaigroup.com>
2016-06-08 16:37:57 +0800

    --

--- boot/src/main/java/com/guohuai/asset/manage/boot/system/config/risk/warning/RiskWarningDao.java
+++ boot/src/main/java/com/guohuai/asset/manage/boot/system/config/risk/warning/RiskWarningDao.java
@@ -1,8 +1,11 @@
 package com.guohuai.asset.manage.boot.system.config.risk.warning;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface RiskWarningDao extends JpaRepository<RiskWarning, String>, JpaSpecificationExecutor<RiskWarning> {
 
+	public List<RiskWarning> findByState(String state);
 }
