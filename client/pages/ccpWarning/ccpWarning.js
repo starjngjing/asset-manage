133c2a5ad080b5a2ba61af4d508d803e51891e63
vania910 <wangyi@guohuaigroup.com>
2016-06-14 14:51:47 +0800

    优化js

--- client/pages/ccpWarning/ccpWarning.js
+++ client/pages/ccpWarning/ccpWarning.js
@@ -258,13 +258,16 @@ define([
 			});
 
 			$('#saveButton').on('click', function() {
-				var f = $('#addForm').validator('doSubmitCheck');
-				var x = $('#addFormOptions').children();
+				var res = [];// 各个form校验结果
+				res.push($('#addForm').validator('doSubmitCheck')); // 校验主form
+				var x = $('#addFormOptions').children(); // 指标项配置
 				for (var i = 0; i < x.length; i++) {
 					var frm = x[i];
-					f = $(frm).validator('doSubmitCheck');
+					res.push($(frm).validator('doSubmitCheck')); // 校验指标项配置form
 				}
-				if(!f)return;
+				for (var i = 0; i < res.length; i++) {
+					if(!res[i])return;
+				}
 				
 				var json = {
 					options: []
@@ -293,13 +296,16 @@ define([
 			});
 
 			$('#updateButton').on('click', function() {
-				var f = $('#updateForm').validator('doSubmitCheck');
-				var x = $('#updateFormOptions').children();
+				var res = [];// 各个form校验结果
+				res.push($('#updateForm').validator('doSubmitCheck')); // 校验主form
+				var x = $('#updateFormOptions').children(); // 指标项配置
 				for (var i = 0; i < x.length; i++) {
 					var frm = x[i];
-					f = $(frm).validator('doSubmitCheck');
+					res.push($(frm).validator('doSubmitCheck')); // 校验指标项配置form
 				}
-				if(!f)return;
+				for (var i = 0; i < res.length; i++) {
+					if(!res[i])return;
+				}
 				
 				var json = {
 					options: []
