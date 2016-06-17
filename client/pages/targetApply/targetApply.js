66bf5451ead6a543c6324ec60f231a31c5a66db3
vania910 <wangyi@guohuaigroup.com>
2016-06-17 16:08:33 +0800

    modify  files

--- client/pages/targetApply/targetApply.js
+++ client/pages/targetApply/targetApply.js
@@ -1067,16 +1067,16 @@ define([
 		if (t) {
 			var t2 = {};
 			$.extend(t2, t); //合并对象，修改第一个对象
-			t2.expAror = t2.expAror ? (t2.expAror*100).toFixed(2) + '%' : "";
+			t2.expAror = t2.expAror ? (t2.expAror * 100).toFixed(2) + '%' : "";
 			t2.collectIncomeRate = t2.collectIncomeRate ? t2.collectIncomeRate.toFixed(2) + '%' : "";
-			
+	
 			t2.raiseScope = t2.raiseScope + '万';
-			t2.life = t2.life + util.enum.transform('lifeUnit',t2.lifeUnit);
+			t2.life = t2.life + util.enum.transform('lifeUnit', t2.lifeUnit);
 			t2.floorVolume = t2.floorVolume + '元';
 			t2.contractDays = t2.contractDays + '天/年';
 			t2.collectDate = t2.collectStartDate + " 至 " + t2.collectEndDate
 			t2.riskRate = util.table.formatter.convertRisk(t2.riskRate); // 格式化风险等级
-
+	
 			return t2;
 		}
 		return t;
