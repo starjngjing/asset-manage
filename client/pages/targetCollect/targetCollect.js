1a1e8d4920bcd7ee6230cb9adb7a492c2f17634d
lirong1986 <lirong@guohuaigroup.com>
2016-06-20 15:11:49 +0800

    --

--- client/pages/targetCollect/targetCollect.js
+++ client/pages/targetCollect/targetCollect.js
@@ -184,7 +184,19 @@ define([
 			}
 			//数据采集按钮
 			$('#riskCollectSubmit').on('click', function() {
-				$('#riskCollectForm').ajaxSubmit({
+					var style = $('#riskWaringDataInput').css('display')
+					if (style == 'block') {
+						var val = $('#riskWaringDataInput').val()
+						if(val.trim() == ""){
+							alert("风险指标数据不能为空!")
+							return
+						}
+						if(isNaN(val)){
+							alert("风险指标数据必须为数字!")
+							return
+						}
+				}
+				return $('#riskCollectForm').ajaxSubmit({
 					url: config.api.system.config.ccr.warning.collect.add,
 					success: function(result) {
 						$('#targetDetailModal').modal('hide');
@@ -193,240 +205,240 @@ define([
 				})
 			})
 
-			//分页配置
-			var pageOptions = {
-				number: 1,
-				size: 10,
-				targetName: '',
-				searchSn: '',
-				state: ''
-			}
+		//分页配置
+		var pageOptions = {
+			number: 1,
+			size: 10,
+			targetName: '',
+			searchSn: '',
+			state: ''
+		}
 
-			// 数据表格配置
-			var tableConfig = {
-				ajax: function(origin) {
-					http.post(config.api.system.config.ccr.warning.collect.list, {
-						data: {
-							page: pageOptions.number,
-							rows: pageOptions.size,
-							name: pageOptions.targetName,
-							sn: pageOptions.searchSn,
-							state: pageOptions.state
-						},
-						contentType: 'form'
-					}, function(rlt) {
-						origin.success(rlt)
-					})
-				},
-				pageNumber: pageOptions.number,
-				pageSize: pageOptions.size,
-				pagination: true,
-				sidePagination: 'server',
-				pageList: [10, 20, 30, 50, 100],
-				queryParams: getQueryParams,
-				detailView: true,
-				onExpandRow: function(index, row, $detail) {
-					$detail.append('<p class="form-header">风险详情</p>')
-					var detTable = $('<table><thead><tr>' +
-						'<th>预警指标</th>' +
-						'<th>风险指标</th>' +
-						'<th>风险等级</th>' +
-						'<th>处置状态</th>' +
-						'</tr></thead></table>')
-					var detTableConfig = {
-						ajax: function(origin) {
-							http.post(config.api.system.config.ccr.warning.collect.detail, {
-								data: {
-									oid: row.oid
-								},
-								contentType: 'form'
-							}, function(rlt) {
-								origin.success(rlt)
-							})
-						},
-						columns: [{
-							field: 'title',
-							align: 'center'
-						}, {
-							field: 'riskData',
-							align: 'center',
-							formatter: function(val, row) {
-								return val + row.riskUnit;
-							}
-						}, {
-							field: 'riskLevel',
-							align: 'center',
-							formatter: function(val) {
-								return util.table.formatter.convertRiskLevel(val);
-							}
-						}, {
-							field: 'handleLevel',
-							align: 'center',
-							formatter: function(val) {
-								if (!val)
-									return '未处置'
-								return '已处置';
-							}
-						}]
-					}
-					$detail.append(detTable)
-					$(detTable).bootstrapTable(detTableConfig)
-				},
-				columns: [{
-					field: 'sn'
-				}, {
-					field: 'name'
-				}, {
-					field: 'state',
-					formatter: function(val) {
-						return util.enum.transform('targetLifeStates', val);
-					}
-				}, {
-					field: 'level',
-					formatter: function(val) {
-						return util.table.formatter.convertRiskLevel(val);
-					}
-				}, {
-					align: 'center',
-					formatter: function(val, row) {
-						var buttons = [{
-							text: '风险采集',
-							type: 'button',
-							class: 'item-add',
-							isRender: true
-						}, {
-							text: '处置历史',
-							type: 'button',
-							class: 'item-his',
-							isRender: true
-						}];
-						return util.table.formatter.generateButton(buttons);
+		// 数据表格配置
+		var tableConfig = {
+			ajax: function(origin) {
+				http.post(config.api.system.config.ccr.warning.collect.list, {
+					data: {
+						page: pageOptions.number,
+						rows: pageOptions.size,
+						name: pageOptions.targetName,
+						sn: pageOptions.searchSn,
+						state: pageOptions.state
 					},
-					events: {
-						'click .item-his': function(e, value, row) {
-							$('#targetHandleHisTable').bootstrapTable('destroy')
-								//分页配置
-							var pageOptions2 = {
-									number: 1,
-									size: 10,
-
-								}
-								// 数据表格配置
-							var tableConfig2 = {
-								ajax: function(origin) {
-									http.post(config.api.system.config.ccr.warning.handle.hisList, {
-										data: {
-											page: pageOptions2.number,
-											rows: pageOptions2.size,
-											oid: row.oid,
-										},
-										contentType: 'form'
-									}, function(rlt) {
-										origin.success(rlt)
-									})
-								},
-								pageNumber: pageOptions2.number,
-								pageSize: pageOptions2.size,
-								pagination: true,
-								sidePagination: 'server',
-								pageList: [10, 20, 30, 50, 100],
-								columns: [{
-									field: 'riskType'
-								}, {
-									field: 'riskName'
-								}, {
-									field: 'riskDet'
-								}, {
-									field: 'handle',
-									align: 'center',
-									formatter: function(val) {
-										return util.enum.transform('warningHandleType', val);
-									}
-
-								}, {
-									field: 'createTime',
-									align: 'center'
-								}, {
-									align: 'center',
-									formatter: function(val, row) {
-										var buttons = [{
-											text: '预警报告',
-											type: 'button',
-											class: 'item-report',
-											isRender: row.report != null
-										}, {
-											text: '过会纪要',
-											type: 'button',
-											class: 'item-meeting',
-											isRender: row.meeting != null
-										}, {
-											text: '处置备忘录',
-											type: 'button',
-											class: 'item-summary',
-											isRender: true
-										}];
-										return util.table.formatter.generateButton(buttons);
-									},
-									events: {
-										'click .item-summary': function(e, value, row) {
-											if (row.summary == "") {
-												$('#summary').html('无')
-											} else {
-												$('#summary').html(row.summary)
-											}
-											$('#summaryDetailModal').modal('show');
-										},
-										'click .item-report': function(e, value, row) {
-											location.href = 'http://api.guohuaigroup.com' + row.report
-										},
-										'click .item-meeting': function(e, value, row) {
-											location.href = 'http://api.guohuaigroup.com' + row.meeting
-										}
-									}
-								}]
-							}
-							$('#targetHandleHisTable').bootstrapTable(tableConfig2)
-							$("#targetHandleHisModal").modal('show')
-						},
-						'click .item-add': function(e, value, row) {
-							http.post(config.api.targetDetQuery, {
-								data: {
-									oid: row.oid
-								},
-								contentType: 'form'
-							}, function(result) {
-								util.form.reset($('#riskCollectForm')); // 先清理表单
-								var data = result.investment;
-								data.raiseScope = data.raiseScope + '万';
-								data.life = data.life + util.enum.transform('lifeUnit', data.lifeUnit);
-								data.expAror = data.expAror + '%';
-								data.floorVolume = data.floorVolume + '元';
-								data.contractDays = data.contractDays + '天/年';
-								data.collectDate = data.collectStartDate + " 至 " + data.collectEndDate
-								data.riskRate = util.table.formatter.convertRisk(data.riskRate); // 格式化风险等级
-								$$.detailAutoFix($('#detTargetForm'), data); // 自动填充详情
-								$('#targetOid').val(data.oid)
-								$('#targetDetailModal').modal('show');
-							})
+					contentType: 'form'
+				}, function(rlt) {
+					origin.success(rlt)
+				})
+			},
+			pageNumber: pageOptions.number,
+			pageSize: pageOptions.size,
+			pagination: true,
+			sidePagination: 'server',
+			pageList: [10, 20, 30, 50, 100],
+			queryParams: getQueryParams,
+			detailView: true,
+			onExpandRow: function(index, row, $detail) {
+				$detail.append('<p class="form-header">风险详情</p>')
+				var detTable = $('<table><thead><tr>' +
+					'<th>预警指标</th>' +
+					'<th>风险指标</th>' +
+					'<th>风险等级</th>' +
+					'<th>处置状态</th>' +
+					'</tr></thead></table>')
+				var detTableConfig = {
+					ajax: function(origin) {
+						http.post(config.api.system.config.ccr.warning.collect.detail, {
+							data: {
+								oid: row.oid
+							},
+							contentType: 'form'
+						}, function(rlt) {
+							origin.success(rlt)
+						})
+					},
+					columns: [{
+						field: 'title',
+						align: 'center'
+					}, {
+						field: 'riskData',
+						align: 'center',
+						formatter: function(val, row) {
+							return val + row.riskUnit;
 						}
+					}, {
+						field: 'riskLevel',
+						align: 'center',
+						formatter: function(val) {
+							return util.table.formatter.convertRiskLevel(val);
+						}
+					}, {
+						field: 'handleLevel',
+						align: 'center',
+						formatter: function(val) {
+							if (!val)
+								return '未处置'
+							return '已处置';
+						}
+					}]
+				}
+				$detail.append(detTable)
+				$(detTable).bootstrapTable(detTableConfig)
+			},
+			columns: [{
+				field: 'sn'
+			}, {
+				field: 'name'
+			}, {
+				field: 'state',
+				formatter: function(val) {
+					return util.enum.transform('targetLifeStates', val);
+				}
+			}, {
+				field: 'level',
+				formatter: function(val) {
+					return util.table.formatter.convertRiskLevel(val);
+				}
+			}, {
+				align: 'center',
+				formatter: function(val, row) {
+					var buttons = [{
+						text: '风险采集',
+						type: 'button',
+						class: 'item-add',
+						isRender: true
+					}, {
+						text: '处置历史',
+						type: 'button',
+						class: 'item-his',
+						isRender: true
+					}];
+					return util.table.formatter.generateButton(buttons);
+				},
+				events: {
+					'click .item-his': function(e, value, row) {
+						$('#targetHandleHisTable').bootstrapTable('destroy')
+							//分页配置
+						var pageOptions2 = {
+								number: 1,
+								size: 10,
+
+							}
+							// 数据表格配置
+						var tableConfig2 = {
+							ajax: function(origin) {
+								http.post(config.api.system.config.ccr.warning.handle.hisList, {
+									data: {
+										page: pageOptions2.number,
+										rows: pageOptions2.size,
+										oid: row.oid,
+									},
+									contentType: 'form'
+								}, function(rlt) {
+									origin.success(rlt)
+								})
+							},
+							pageNumber: pageOptions2.number,
+							pageSize: pageOptions2.size,
+							pagination: true,
+							sidePagination: 'server',
+							pageList: [10, 20, 30, 50, 100],
+							columns: [{
+								field: 'riskType'
+							}, {
+								field: 'riskName'
+							}, {
+								field: 'riskDet'
+							}, {
+								field: 'handle',
+								align: 'center',
+								formatter: function(val) {
+									return util.enum.transform('warningHandleType', val);
+								}
+
+							}, {
+								field: 'createTime',
+								align: 'center'
+							}, {
+								align: 'center',
+								formatter: function(val, row) {
+									var buttons = [{
+										text: '预警报告',
+										type: 'button',
+										class: 'item-report',
+										isRender: row.report != null
+									}, {
+										text: '过会纪要',
+										type: 'button',
+										class: 'item-meeting',
+										isRender: row.meeting != null
+									}, {
+										text: '处置备忘录',
+										type: 'button',
+										class: 'item-summary',
+										isRender: true
+									}];
+									return util.table.formatter.generateButton(buttons);
+								},
+								events: {
+									'click .item-summary': function(e, value, row) {
+										if (row.summary == "") {
+											$('#summary').html('无')
+										} else {
+											$('#summary').html(row.summary)
+										}
+										$('#summaryDetailModal').modal('show');
+									},
+									'click .item-report': function(e, value, row) {
+										location.href = 'http://api.guohuaigroup.com' + row.report
+									},
+									'click .item-meeting': function(e, value, row) {
+										location.href = 'http://api.guohuaigroup.com' + row.meeting
+									}
+								}
+							}]
+						}
+						$('#targetHandleHisTable').bootstrapTable(tableConfig2)
+						$("#targetHandleHisModal").modal('show')
+					},
+					'click .item-add': function(e, value, row) {
+						http.post(config.api.targetDetQuery, {
+							data: {
+								oid: row.oid
+							},
+							contentType: 'form'
+						}, function(result) {
+							util.form.reset($('#riskCollectForm')); // 先清理表单
+							var data = result.investment;
+							data.raiseScope = data.raiseScope + '万';
+							data.life = data.life + util.enum.transform('lifeUnit', data.lifeUnit);
+							data.expAror = data.expAror + '%';
+							data.floorVolume = data.floorVolume + '元';
+							data.contractDays = data.contractDays + '天/年';
+							data.collectDate = data.collectStartDate + " 至 " + data.collectEndDate
+							data.riskRate = util.table.formatter.convertRisk(data.riskRate); // 格式化风险等级
+							$$.detailAutoFix($('#detTargetForm'), data); // 自动填充详情
+							$('#targetOid').val(data.oid)
+							$('#targetDetailModal').modal('show');
+						})
 					}
-				}]
-			}
+				}
+			}]
+		}
 
-			$('#targetCollectTable').bootstrapTable(tableConfig)
-				// 搜索表单初始化
-			$$.searchInit($('#targetSearchForm'), $('#targetCollectTable'))
+		$('#targetCollectTable').bootstrapTable(tableConfig)
+		// 搜索表单初始化
+		$$.searchInit($('#targetSearchForm'), $('#targetCollectTable'))
 
-			function getQueryParams(val) {
-				var form = document.targetSearchForm
-				pageOptions.size = val.limit
-				pageOptions.number = parseInt(val.offset / val.limit) + 1
-				pageOptions.targetName = form.searchName.value.trim();
-				pageOptions.searchSn = form.searchSn.value.trim();
-				pageOptions.state = form.targetStatus.value.trim();
-				return val
-			}
+		function getQueryParams(val) {
+			var form = document.targetSearchForm
+			pageOptions.size = val.limit
+			pageOptions.number = parseInt(val.offset / val.limit) + 1
+			pageOptions.targetName = form.searchName.value.trim();
+			pageOptions.searchSn = form.searchSn.value.trim();
+			pageOptions.state = form.targetStatus.value.trim();
+			return val
 		}
 	}
+}
 
 })
